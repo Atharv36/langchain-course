bad185558be689dad6a930399d997366cbac6875
Atharv36 <94675535+Atharv36@users.noreply.github.com>
2026-01-27 22:43:22 +0530

    replaced pydantic parsing with with_structured_parsing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from langchain_classic.agents.react.agent import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
+# from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 
@@ -21,11 +21,14 @@ from schemas import AgentRespone
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4o-mini")
 # react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object = AgentRespone)
+# output_parser = PydanticOutputParser(pydantic_object = AgentRespone)
+
+structured_llm = llm.with_structured_output(AgentRespone)
+
 react_prompt_with_format_instructions = PromptTemplate(
     template = REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables = ["input","agent_scratchpad","tool_names"]
-).partial(format_instructions = output_parser.get_format_instructions())
+).partial(format_instructions = "")
 
 
 agent = create_react_agent(
@@ -38,11 +41,11 @@ agent = create_react_agent(
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True,handle_parsing_errors=True
 )
 extract_output = RunnableLambda(lambda x : x["output"])
-parse_output = RunnableLambda(lambda x : output_parser.parse(x))
+# parse_output = RunnableLambda(lambda x : output_parser.parse(x))
 
 
 
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():

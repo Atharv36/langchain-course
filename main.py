8431bdb33af46cdeececbd9c6b987db5cca3d248
Atharv36 <94675535+Atharv36@users.noreply.github.com>
2026-01-29 20:37:52 +0530

    init

--- main.py
+++ main.py
@@ -3,60 +3,146 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain_classic import hub
-from langchain_classic.agents import AgentExecutor
-from langchain_classic.agents.react.agent import create_react_agent
+
+from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
 # from langchain_core.output_parsers.pydantic import PydanticOutputParser
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableLambda
+# from langchain_core.prompts import PromptTemplate
+# from langchain_core.runnables import RunnableLambda
 
-from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+# from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentRespone
 
 
 
 tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o-mini")
+model = ChatOpenAI(model="gpt-4o-mini")
 # react_prompt = hub.pull("hwchase17/react")
 # output_parser = PydanticOutputParser(pydantic_object = AgentRespone)
 
-structured_llm = llm.with_structured_output(AgentRespone)
+# structured_llm = llm.with_structured_output(AgentRespone)
 
-react_prompt_with_format_instructions = PromptTemplate(
-    template = REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables = ["input","agent_scratchpad","tool_names"]
-).partial(format_instructions = "")
+# react_prompt_with_format_instructions = PromptTemplate(
+#     template = REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+#     input_variables = ["input","agent_scratchpad","tool_names"]
+# ).partial(format_instructions = "")
 
 
-agent = create_react_agent(
+agent = create_agent(
+    model=model,
     tools=tools,
-    llm=llm,
-    prompt=react_prompt_with_format_instructions,
+    # prompt=react_prompt_with_format_instructions,
+    response_format = AgentRespone
 )
 
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True,handle_parsing_errors=True
-)
-extract_output = RunnableLambda(lambda x : x["output"])
+# agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True,handle_parsing_errors=True
+
+# extract_output = RunnableLambda(lambda x : x["output"])
 # parse_output = RunnableLambda(lambda x : output_parser.parse(x))
 
 
 
-chain = agent_executor | extract_output | structured_llm
+# chain = agent_executor | extract_output | structured_llm
 
 
 def main():
     print("Hello from langchain-course!")
-    result = chain.invoke(
-        input={
-            "input": "Top Goal Scorer in Soccerr in entire history of the sport with number of goals and assists"
-        }
-    )
-    print(result)
+    # result = chain.invoke(
+    #     input={
+    #         "input": "Top Goal Scorer in Soccerr in entire history of the sport with number of goals and assists"
+    #     }
+    # )
+
+    result = agent.invoke({
+        "messages":[{
+            "role":"user",
+            "content":"Top Goal Scorer in Soccerr in entire history of the sport with number of goals and assists",
+
+        }]
+    })
+    print(result["structured_response"])
 
 
 if __name__ == "__main__":
     main()
+
+
+# # from langchain_classic.agents import Agent
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# from langchain_classic import hub
+# from langchain_classic.agents import AgentExecutor
+# from langchain_classic.agents.react.agent import create_react_agent
+# from langchain_openai import ChatOpenAI
+# from langchain_tavily import TavilySearch
+
+# # from langchain_core.output_parsers.pydantic import PydanticOutputParser
+# from langchain_core.prompts import PromptTemplate
+# from langchain_core.runnables import RunnableLambda
+
+# from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+# from schemas import AgentRespone
+
+
+
+# tools = [TavilySearch()]
+# llm = ChatOpenAI(model="gpt-4o-mini")
+# # react_prompt = hub.pull("hwchase17/react")
+# # output_parser = PydanticOutputParser(pydantic_object = AgentRespone)
+
+# structured_llm = llm.with_structured_output(AgentRespone)
+
+# react_prompt_with_format_instructions = PromptTemplate(
+#     template = REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+#     input_variables = ["input","agent_scratchpad","tool_names"]
+# ).partial(format_instructions = "")
+
+
+# agent = create_react_agent(
+#     tools=tools,
+#     llm=llm,
+#     prompt=react_prompt_with_format_instructions,
+# )
+
+
+# agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True,handle_parsing_errors=True
+# )
+# extract_output = RunnableLambda(lambda x : x["output"])
+# # parse_output = RunnableLambda(lambda x : output_parser.parse(x))
+
+
+
+# chain = agent_executor | extract_output | structured_llm
+
+
+# def main():
+#     print("Hello from langchain-course!")
+#     result = chain.invoke(
+#         input={
+#             "input": "Top Goal Scorer in Soccerr in entire history of the sport with number of goals and assists"
+#         }
+#     )
+#     print(result)
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
